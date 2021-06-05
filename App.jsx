2cd93718d65db96f651b1bb67d6a18fdf58436cf
saimanojakula <56781040+saimanojakula@users.noreply.github.com>
2021-06-05 13:16:40 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="note">
+      <h1>Legendary Shiny's</h1>
+      <p>Buy the lvl-100, Fully Up-Graded Pokemon.</p>
+    </div>
+  );
+}
+
+export default Card;
